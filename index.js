8e0a4df9314be463a7a8852ffd2b98e055fe493c
Juuha <juha.ritakoski@hotmail.com>
2018-11-28 16:13:06 +0200

    tehtävät 4.1 ja 4. 4.2 tehty

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+}
+
+mongoose
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then( () => {
+    console.log('Connected to database.')
+  })
+  .catch( error => {
+    console.log(error)
+  })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('/api/blogs', blogsRouter)
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
