d53e3e0a71174a270680f5a4f87022d78409ac1b
Juuha <juha.ritakoski@hotmail.com>
2018-12-07 15:47:52 +0200

    tehtävät 4.15-4.21 tehty

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,35 +1,75 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
-  response.json(blogs)
+  const blogs = await Blog
+    .find({})
+    .populate('user', { _id: 1, username: 1, name: 1 })
+
+  response.json(blogs.map(Blog.format))
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const body = request.body
-  
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes || 0
-  })
+  try {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
-  if (blog.title === undefined || blog.url === undefined) {
-    return response.status(400).json({ error: 'title or url missing' })
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid or missing' })
+    }
+
+    const body = request.body
+    const user = await User.findById(decodedToken.id)
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0,
+      user: user._id
+    })
+
+    if (blog.title === undefined || blog.url === undefined) {
+      return response.status(400).json({ error: 'title or url missing' })
+    }
+
+    const savedBlog = await blog.save()
+
+    user.blogs = user.blogs.concat(savedBlog)
+    await user.save()
+
+    response.status(201).json(savedBlog)
+  } catch (exception) {
+    if (exception.name === 'JsonWebTokenError'){
+      return response.status(401).json({ error: exception.message })
+    }
+    console.log(exception)
+    response.status(500).json({ error: 'Something went wrong.' })
   }
-
-  const savedBlog = await blog.save()
-  response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
   try {
-    await Blog.findByIdAndDelete(request.params.id)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid or missing' })
+    }
+
+    const blog = await Blog.findById(request.params.id)
+
+    if (blog.user.toString() !== decodedToken.id.toString()){
+      return response.status(401).json({ error: 'Only the poster of the blog can delete it.' })
+    }
+
+    await blog.delete()
 
     response.status(204).end()
   } catch (exception) {
+    if (exception.name === 'JsonWebTokenError'){
+      return response.status(401).json({ error: exception.message })
+    }
     console.log(exception)
     response.status(400).send({ error: 'malformatted id' })
   }
--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const config =require('./utils/config')
 
 if (process.env.NODE_ENV !== 'production'){
@@ -22,8 +25,11 @@ mongoose
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 const server = http.createServer(app)
 
